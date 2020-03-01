7540b68e6c0a3de082996dfdea4b88be98fb87c8
Daythehangedman <59592689+Daythehangedman@users.noreply.github.com>
2020-03-02 00:45:44 +0800

    using 2 panes

---
+++ src/main/java/Main.java
@@ -0,0 +1,28 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
+            AnchorPane root = fxmlLoader.load();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.setTitle("Tic-Tac-Toe");
+            stage.setResizable(false);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/MainWindow.java
@@ -0,0 +1,27 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+
+public class MainWindow extends AnchorPane {
+
+    @FXML
+    private Button resetButton;
+
+    @FXML
+    private GridPane boardPane;
+
+    @FXML
+    private Pane leftPane;
+
+    @FXML
+    private Pane rightPane;
+
+
+
+
+
+
+
+}
